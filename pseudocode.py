198d245ce78f0f86af3d4e0d46ebd7357f9cb76f
QX1207 <54901364+QX1207@users.noreply.github.com>
2020-01-30 09:30:36 -0600

    Update #1 It is a beginning!!!
    Created main.py, changelog.md, and pseudocode.py.
    Begin to write my pseudocode that I can make a draft about my project.

---
+++ pseudocode.py
@@ -0,0 +1,29 @@
+# Course: CS 30
+# Period: 1
+# Date create: 20/01/30
+# Date last modified: 20/01/30
+# Name: Qian Xiang
+# Description: psceudocodes for this game
+
+
+# Print out a welcome statement
+# Two selections:
+# First: Just print the sentence "Welcome to the Magic Tower"
+# I would like to print it slowly that create a little bit visual
+# effect
+# import sys
+# import time
+
+# def print_slow(str):
+#     for char in str:
+#         time.sleep(.3)
+#         sys.stdout.write(char)
+#         sys.stdout.flush()
+#
+# print_slow('Welcome to the Magic Tower')
+# Second: Print out the following code
+# "      ______     "
+# "    /  ___  \    "
+# "   /  /  \   \   "
+# " /  /     \   \/  "
+# "/__/       \_____/"
