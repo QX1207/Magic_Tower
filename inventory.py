842c8bd92bcf0a8efd86505b7f72c0d41388f691
QX1207 <54901364+QX1207@users.noreply.github.com>
2020-02-13 09:15:12 -0600

    Puted a header in my inventory.py
    I puted a header in my inventory.py that I can manage my todo list easier after time.

---
+++ inventory.py
@@ -0,0 +1,7 @@
+# Course: CS 30
+# Period: 1
+# Date created: 20/02/13
+# Date last modified: 20/02/13
+# Name: Qian Xiang
+# Description: inventory for this game that store my weapon's name, data, and
+# description
